6f03a4d578e6213be841a37d84545e46eceb8ccf
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-12 19:54:17 +0300

    first layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="app-wrapper">
             <header className="header">
-                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324"/>
+                <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/business-logo-design-template-78655edda18bc1196ab28760f1535baa_screen.jpg?ts=1617645324" alt="img"/>
             </header>
             <nav className="nav">
                 <div>
