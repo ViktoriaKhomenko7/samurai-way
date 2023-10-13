10807450365468e90d3de084823666120eec1c87
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-14 00:18:17 +0300

    create components

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../images/planet.png'
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img src={logo} alt="img"/>
+        </header>
+    );
+};
