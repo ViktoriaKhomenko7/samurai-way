0bfe30196056d80cecf65b6a28f2fb68ef21fd81
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-11-07 00:33:17 +0300

    react-router-dom is installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Header} from "./components/header/Header";
 import {Navbar} from "./components/navbar/Navbar";
-import {Profile} from "./components/propfile/Profile";
 import {Dialogs} from "./components/dialogs/Dialogs";
+import {Profile} from "./components/propfile/Profile";
+import {News} from "./components/news/News";
+import {Music} from "./components/music/Music";
+import {Settings} from "./components/settings/Settings";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={"app-wrapper-content"}>
-                <Dialogs/>
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className={"app-wrapper-content"}>
+                    <Routes>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
